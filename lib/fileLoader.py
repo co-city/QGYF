f5ed2d0e77abb5481e866d66dba045fb7ab64202
erikedmark <erik.j.edmark@gmail.com>
2019-03-22 17:56:47 +0100

    Read layers from dxf files

--- lib/fileLoader.py
+++ lib/fileLoader.py
@@ -5,13 +5,11 @@ from PyQt5.QtCore import QVariant
 from qgis.core import QgsProject, QgsVectorLayer, QgsWkbTypes, QgsFields, QgsField
 
 def loadFile(interface):
+  """  
+  Load file and add features to input layers of matching type (Point, Line, Polygon).
+  @param {QtWidget} interface
   """
-  
-  Load file and add features to input layers of matching type (Point, Line, Polygon)
-
-  @param {QtWidget} interce
-  """
-  file = QFileDialog.getOpenFileName(interface, 'Öppna fil', '', '*.shp')
+  file = QFileDialog.getOpenFileName(interface, 'Öppna fil', '', '*.shp; *.dxf')
   filePath = file[0]
   fileName = os.path.basename(filePath)
   extension = os.path.splitext(fileName)[1]
@@ -25,37 +23,40 @@ def loadFile(interface):
   
   pointLayer.startEditing()
   lineLayer.startEditing()
-  polygonLayer.startEditing()
+  polygonLayer.startEditing()  
+
+  layers = []
 
   for feature in features:
+    if extension == ".shp":
+      mapShapeAttributes(feature.attributes)
+    if extension == ".dxf":
+      lookupLayer(feature, layers)
+
     geom = feature.geometry()
     geom.convertToSingleType()
     feature.setGeometry(geom);
-    type = QgsWkbTypes.geometryDisplayString(geom.type())
+    type = QgsWkbTypes.geometryDisplayString(geom.type())    
     if type == "Point":  
-      addFeature(feature, type, fileName, pointLayer)  
+      addFeature(feature, type, fileName, pointLayer, extension)  
     if type == "Line":    
-      addFeature(feature, type, fileName, lineLayer)
+      addFeature(feature, type, fileName, lineLayer, extension)
     if type == "Polygon":    
-      addFeature(feature, type, fileName, polygonLayer)
+      addFeature(feature, type, fileName, polygonLayer, extension)
 
   pointLayer.commitChanges()
   lineLayer.commitChanges()
   polygonLayer.commitChanges()
+  print(layers)
 
-def addFeature(feature, type, fileName, layer):
-  """
-  
+def addFeature(feature, type, fileName, layer, extension):
+  """  
   Convert attributes and add features to input layer.
-
   @param {QgsFeature} feature
   @param {string} type
   @param {string} fileName
   @param {QgsVectorLayer} layer
-  """
-  fields = feature.fields()
-  attributes = feature.attributes()
-
+  """  
   fields = QgsFields()
   fields.append(QgsField("id", QVariant.Int, "serial"))
   fields.append(QgsField("filnamn", QVariant.String, "text"))
@@ -65,3 +66,18 @@ def addFeature(feature, type, fileName, layer):
   feature.setAttributes([None, fileName, ""])
 
   layer.addFeature(feature)
+
+def mapShapeAttributes(feature, layers):
+  return True
+
+def lookupLayer(feature, layers):
+  """
+  Lookup features layername and append to the layer list if is a new one.
+  @param {QgsFeature} feature
+  @param {list<str>} layers
+  """
+  index = feature.fields().indexFromName("Layer")
+  layer = feature.attributes()[index]
+  layer = layer.encode("windows-1252").decode("utf-8")    
+  if layer not in layers:
+    layers.append(layer)
